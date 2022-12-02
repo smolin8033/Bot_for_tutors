14919dd242f23e03b26f086f8b93f01a015dbbca
smoll <smolll123@mail.ru>
2022-12-03 02:42:03 +0300

    fix: need to set up aiohttp

--- handlers/sent_api/registration.py
+++ handlers/sent_api/registration.py
@@ -1,6 +1,8 @@
-import requests
+import asyncio
 
+import aiohttp
 
+"""
 def send_registration_data(user_id, username):
     url = 'http://localhost/api/telegram_users/'
     data = {
@@ -9,3 +11,16 @@ def send_registration_data(user_id, username):
     }
     result = requests.post(url=url, data=data)
     return result
+"""
+
+async def send_registration_data(user_id, username):
+    async with aiohttp.ClientSession() as session:
+        async with session.post('http://localhost/api/telegram_users/', data={
+        "username": username,
+        "telegram_id": user_id
+    }) as resp:
+            print(resp.status)
+            print(await resp.text())
+
+
+asyncio.run(send_registration_data())
--- loader.py
+++ loader.py
@@ -2,13 +2,12 @@ from aiogram import Bot, Dispatcher
 
 from config.conf import settings
 
-# TODO poetry вместо requirements
 # TODO async запросы только можно httpx/aiohttp
+# TODO data transfer object (создать Dataclass User поля username telegram_id) dataclasses python
 # TODO make timeout python
 # TODO обработка ответа вы зарегистрировались, вы не зарегистрировались
 # TODO залогировать такой-то пользователь залогировался или нет
 # TODO url вынести в настройки
-# TODO data transfer object (создать Dataclass User поля username telegram_id) dataclasses python
 # TODO api папка там файл с отправкой api
 # TODO если можно tests если найти как делать
 # TODO при регистрации имя фамилия (либо учитель, либо ученик)
