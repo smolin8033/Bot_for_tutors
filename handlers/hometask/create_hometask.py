ea8efe027b184f2e844dc63d391df282ee2dfd30
smoll <smolll123@mail.ru>
2023-07-11 21:04:37 +0300

    feat: write calendar data into states

--- handlers/hometask/create_hometask.py
+++ handlers/hometask/create_hometask.py
@@ -3,7 +3,6 @@ from aiogram.dispatcher import FSMContext
 from aiogram.types import CallbackQuery
 from aiogram_calendar import SimpleCalendar, simple_cal_callback
 
-from config.logging_conf.loggers import logger
 from loader import bot, dp
 from states.hometask import HometaskStatesGroup
 
@@ -26,22 +25,49 @@ async def load_name(message: types.Message, state: FSMContext) -> None:
 
 
 @dp.callback_query_handler(simple_cal_callback.filter(), state=HometaskStatesGroup.start_datetime)
-async def process_simple_calendar(callback_query: CallbackQuery, callback_data: dict, state: FSMContext) -> None:
+async def process_start_datetime(callback_query: CallbackQuery, callback_data: dict, state: FSMContext) -> None:
     selected, date = await SimpleCalendar().process_selection(callback_query, callback_data)
 
     if selected:
         async with state.proxy() as data:
             data["start_datetime"] = date
 
-        await callback_query.message.answer("Now, when is the deadline for this homework?")
+        await callback_query.message.answer("Your choice is accepted.")
 
-        logger.error(f"{await state.get_state()}")
         await HometaskStatesGroup.next()
-        logger.error(f"{await state.get_state()}")
+
+        await load_end_datetime(state)
 
 
-@dp.message_handler(state=HometaskStatesGroup.end_datetime)
-async def load_end_datetime(message: types.Message, state: FSMContext) -> None:
+async def load_end_datetime(state: FSMContext) -> None:
+    # async with state.proxy() as data:
+    #     data["end_datetime"] = 'random'
+
+    await bot.send_message(state.chat, "Now, when is the deadline for this homework?")
+    await bot.send_message(state.chat, "Please, select a date: ", reply_markup=await SimpleCalendar().start_calendar())
+
+
+@dp.callback_query_handler(simple_cal_callback.filter(), state=HometaskStatesGroup.end_datetime)
+async def process_end_datetime(callback_query: CallbackQuery, callback_data: dict, state: FSMContext) -> None:
+    selected, date = await SimpleCalendar().process_selection(callback_query, callback_data)
+
+    if selected:
+        async with state.proxy() as data:
+            data["end_datetime"] = date
+
+        await callback_query.message.answer("Your choice is accepted.")
+
+        await HometaskStatesGroup.next()
+
+        await load_coursebook(state)
+
+
+@dp.message_handler(state=HometaskStatesGroup.coursebook)
+async def load_coursebook(message: types.Message, state: FSMContext) -> None:
     async with state.proxy() as data:
-        await bot.send_message(state.chat, data)
-    # await state.finish()
+        data["coursebook"] = message.text
+
+    async with state.proxy() as data:
+        await message.answer(data)
+
+    await HometaskStatesGroup.next()
