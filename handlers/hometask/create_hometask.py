b717898236d29126a62dbce44259105bc6ef87b8
smoll <smolll123@mail.ru>
2023-07-18 21:07:34 +0300

    feat: collected info to create hometask

--- handlers/hometask/create_hometask.py
+++ handlers/hometask/create_hometask.py
@@ -40,8 +40,6 @@ async def process_start_datetime(callback_query: CallbackQuery, callback_data: d
 
 
 async def load_end_datetime(state: FSMContext) -> None:
-    # async with state.proxy() as data:
-    #     data["end_datetime"] = 'random'
 
     await bot.send_message(state.chat, "Now, when is the deadline for this homework?")
     await bot.send_message(state.chat, "Please, select a date: ", reply_markup=await SimpleCalendar().start_calendar())
@@ -57,9 +55,12 @@ async def process_end_datetime(callback_query: CallbackQuery, callback_data: dic
 
         await callback_query.message.answer("Your choice is accepted.")
 
-        await HometaskStatesGroup.next()
+        await ask_coursebook(state)
 
-        await load_coursebook(state)
+
+async def ask_coursebook(state: FSMContext) -> None:
+    await bot.send_message(state.chat, "Write the coursebook, if you have one.")
+    await HometaskStatesGroup.next()
 
 
 @dp.message_handler(state=HometaskStatesGroup.coursebook)
@@ -67,7 +68,34 @@ async def load_coursebook(message: types.Message, state: FSMContext) -> None:
     async with state.proxy() as data:
         data["coursebook"] = message.text
 
+    await message.answer("Write the exercises, if you wish.")
+    await HometaskStatesGroup.next()
+
+
+@dp.message_handler(state=HometaskStatesGroup.exercises)
+async def load_exercises(message: types.Message, state: FSMContext) -> None:
+    async with state.proxy() as data:
+        data["exercises"] = message.text
+
+    await message.answer("You can place the url of the hometask here, if you have one.")
+    await HometaskStatesGroup.next()
+
+
+@dp.message_handler(state=HometaskStatesGroup.url)
+async def load_url(message: types.Message, state: FSMContext) -> None:
+    async with state.proxy() as data:
+        data["url"] = message.text
+
+    await message.answer("Write the additional info, if you wish.")
+    await HometaskStatesGroup.next()
+
+
+@dp.message_handler(state=HometaskStatesGroup.more_info)
+async def load_more_info(message: types.Message, state: FSMContext) -> None:
+    async with state.proxy() as data:
+        data["more_info"] = message.text
+
     async with state.proxy() as data:
         await message.answer(data)
 
-    await HometaskStatesGroup.next()
+    await state.finish()
